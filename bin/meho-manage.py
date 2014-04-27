4b21fac16e44b8b16788b3f4f86174011a58a712
4rch0dia <dimitri.racordon@gmail.com>
2014-04-27 18:23:01 +0200

    Added media_list, media_detial and media_create + bug fix

--- bin/meho-manage.py
+++ bin/meho-manage.py
@@ -15,7 +15,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import sys
 import argparse, json, re, requests
+import getpass
+
 from urllib.parse import urljoin, urlunsplit
 
 __version__ = '0.1'
@@ -33,16 +36,41 @@ class MehoClient(object):
         meho api
         ~~~~~~~~
 
-            meho-manage api version <url>
+            meho-manage api version <api>
 
                 Prints the version of meho Rest API running on the remote
-                server. ``url`` denotes the root url to the API endpoint, e.g.
+                server. ``api`` denotes the root url to the API endpoint, e.g.
                 http://site.com/meho/api/
 
         meho media
         ~~~~~~~~~~
 
-            meho-manage media list <url> [--filters...]
+            meho-manage media list   [-u user] [-p password] [-f filter...]
+                                     <api>
+
+                Prints the ID of all stored media.
+
+            meho-manage media detail [-u user] [-p [password]] <media_urn> <api>
+
+                Prints the all information stored for a specific media.
+
+            meho-manage media create [-u user] [-p [password]] [-i urn] [-t type]
+                                     [-a parent] [-o] <private_url> <api>
+
+                Create a new media and register it in the database.
+
+                ``private_url`` should denote a meho-compatible url to the
+                media source, e.g. file:///path/to/media. See the meho
+                documentation for an exhaustive list of supported url scheme.
+
+                The api will return a HTTP 400 error if you specify an ``urn``
+                which is already being used by another media. Set --overwrite
+                if you want to ignore this error and overwrite any existing
+                media.
+
+            meho-manage media update
+            meho-manage media delete
+
         """
         if len(args) > 1 and args[1] in ('-h', '--help'):
             print(self.main.__doc__)
@@ -69,13 +97,12 @@ class MehoClient(object):
     def api_version(self, *args):
         # parse command line options
         parser = argparse.ArgumentParser(prog='meho-manage api version')
-        parser.add_argument('url', help='root url to the API endpoint')
+        parser.add_argument('api', help='root url to the API endpoint')
         args = parser.parse_args(args)
 
         # request API version on the meho server
-        url = urljoin(self._format_url(args.url), 'version')
-        print(args.url, self._format_url(args.url))
-        r = requests.get(url)
+        endpoint = urljoin(self._format_url(args.api), 'version')
+        r = requests.get(endpoint)
 
         # parse the server response
         data = r.json()
@@ -84,33 +111,108 @@ class MehoClient(object):
     def media_list(self, *args):
         # parse command line options
         parser = argparse.ArgumentParser(prog='meho-manage media list')
-        parser.add_argument('url', help='root url to the API endpoint')
         parser.add_argument('-u', '--user', help='username of your meho account')
         parser.add_argument('-p', '--password', help='password of your meho account')
-        parser.add_argument('-f', '--filters', help='list of filters to apply on list query')
+        parser.add_argument('-f', '--filters', action='append', default=[],
+            help='list of query filters')
+        parser.add_argument('api', help='root url to the API endpoint')
         args = parser.parse_args(args)
 
         # request API for media list
-        api_url = self._format_url(args.url)
-        csrftoken = self._get_csrf_token(api_url)
-        payload = {k:v for k,v in map(lambda f: f.split('='), args.filters)}
-        r = requests.get(url, cookies={'csrftoken':csrftoken}, params=payload)
+        filters = {k:v for k,v in map(lambda f: f.split('='), args.filters)}
+        auth = self._get_credentials(args)
+        endpoint = self._format_url(args.api) + 'media/'
+        r = requests.get(endpoint, auth=auth, params=filters)
 
         # parse the server response
-        data = r.json()
-        print(data)
+        if r.status_code == 200:
+            data = r.json()
+            if data['media']:
+                for media in data['media']:
+                    print(media['urn'])
+            else:
+                print('No stored media')
+        elif r.status_code == 401:
+            self._handle_401(r)
 
-    def _get_csrf_token(self, url):
-        if 'CSRFTOKEN' in os.environ:
-            return os.environ['CSRFTOKEN']
-        url = urljoin(self._format_url(args.url), 'version')
-        r = requests.get(url)
+    def media_detail(self, *args):
+        # parse command line options
+        parser = argparse.ArgumentParser(prog='meho-manage media detail')
+        parser.add_argument('-u', '--user', help='username of your meho account')
+        parser.add_argument('-p', '--password', help='password of your meho account')
+        parser.add_argument('media_urn', help='urn of the media to be detailed')
+        parser.add_argument('api', help='root url to the API endpoint')
+        args = parser.parse_args(args)
+
+        # request API for media detail
+        auth = self._get_credentials(args)
+        api_url = self._format_url(args.api)
+        endpoint = '%(api)smedia/%(urn)s' % {'api': api_url, 'urn': args.media_urn}
+        r = requests.get(endpoint, auth=auth)
+
+        # parse the server response
+        if r.status_code == 200:
+            print(json.dumps(r.json(), indent=4, sort_keys=True))
+        elif r.status_code == 401:
+            self._handle_401(r)
+        elif r.status_code == 404:
+            self._handle_404(r)
+
+    def media_create(self, *args):
+        # parse command line options
+        parser = argparse.ArgumentParser(prog='meho-manage media create')
+        parser.add_argument('-u', '--user', help='username of your meho account')
+        parser.add_argument('-p', '--password', help='password of your meho account')
+        parser.add_argument('-i', '--urn', help='urn of the media (auto-generated by default)')
+        parser.add_argument('-t', '--media_type', help='type of the media (guessed by default)')
+        parser.add_argument('-a', '--parent', help='parent of the media')
+        parser.add_argument('-o', '--overwrite', action='store_true', default=False,
+            help='overwrite existing media')
+        parser.add_argument('private_url', help='private url of the media')
+        parser.add_argument('api', help='root url to the API endpoint')
+        args = parser.parse_args(args)
+
+        # get csrftoken
+        api_url = self._format_url(args.api)
+        session = requests.Session()
+        csrftoken = self._get_csrf_token(session, api_url)
+
+        # forge payload
+        fields = ['urn', 'media_type', 'parent', 'private_url']
+        payload = {'media': {f:getattr(args, f) for f in fields if getattr(args, f) is not None}}
+        payload['overwrite'] = args.overwrite
+
+        # request API for media creation
+        endpoint = api_url + 'media'
+        if args.urn:
+            endpoint += '/' + args.urn
+        auth = self._get_credentials(args)
+        r = session.put(endpoint, auth=auth, data=json.dumps(payload),
+            headers={'X-CSRFToken':csrftoken, 'content-type': 'application/json'})
+
+        # parse the server response
+        if r.status_code == 201:
+            print(json.dumps(r.json(), indent=4, sort_keys=True))
+        elif r.status_code == 401:
+            self._handle_401(r)
+
+    def _handle_401(self, response):
+        print('\033[91mAuthentication failed\033[0m\n'
+            '\tThe request could not be executed. Please check your credentials.\n'
+            '\tServer reponse: %s' % response.text, file=sys.stderr)
+
+    def _handle_404(self, response):
+        print('\033[91mResource not found\033[0m', file=sys.stderr)
+
+    def _get_csrf_token(self, session, api_url):
+        endpoint = urljoin(self._format_url(api_url), 'version')
+        r = session.get(endpoint)
         return r.cookies.get('csrftoken', None)
 
     def _format_url(self, url):
         """Completes missing parts of a user-privided and returns it."""
         exp = re.compile(r'^((?P<scheme>[a-z](?:[a-z\d\+\.-])*):\/\/)?'
-            r'(?P<host>[a-z](?:[a-z\d\.-])*(?::[0-9]+)?)(?P<path>(?:\/[\w\.-]+\/?)*)'
+            r'(?P<host>(?:[a-z\d\.-])*(?::[0-9]+)?)(?P<path>(?:\/[\w\.-]+\/?)*)'
             r'(?P<query>\?(?:\w+(?:=\w+)&?)*)?(?P<fragment>#\w*)?$')
         match = exp.match(url)
         if not match:
@@ -119,6 +221,14 @@ class MehoClient(object):
         parts = match.groupdict('')
         parts['scheme'] = parts['scheme'] or 'http'
         return '%(scheme)s://%(host)s%(path)s%(query)s%(fragment)s' % parts
+
+    def _get_credentials(self, args):
+        if args.user:
+            # if --user is set within the command line but --password is not,
+            # we will ask the user to provide one on stdin
+            pwd = args.password or getpass.getpass('Password:')
+            return (args.user, pwd)
+        return None
 
 if __name__ == '__main__':
     import sys
